595ec8486d026949df03b6f0c6f43788054656be
Muhammad Fadila <muh.fadila18@gmail.com>
2022-12-16 17:35:08 +0700

    add fungtion while

--- Program.cs
+++ Program.cs
@@ -89,6 +89,24 @@ namespace CircularQueues_Csharp
                 }
                 Console.WriteLine();
             }
+            else
+            {
+                /* traverses the queue till the last position of the array. */
+                while (FRONT_position <= max - 1)
+                {
+                    Console.Write(queue_array[FRONT_position] + "    ");
+                    FRONT_position++;
+                }
+                /* set the FRONT position to the first element of the array/ */
+                FRONT_position = 0;
+                /*traverses the array till the last element present in the queue/ */
+                while (FRONT_position <= REAR_position)
+                {
+                    Console.Write(queue_array[FRONT_position] + "   ");
+                    FRONT_position++;
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
