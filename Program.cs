4bdf2016a12a207926bb9bb46934d53e82d00ca1
Muhammad Fadila <muh.fadila18@gmail.com>
2022-12-16 17:29:22 +0700

    add fungtion if

--- Program.cs
+++ Program.cs
@@ -68,6 +68,18 @@ namespace CircularQueues_Csharp
                     FRONT = FRONT + 1;
             }
         }
-        public void display() { }
+        public void display()
+        {
+            int FRONT_position = FRONT;
+            int REAR_position = REAR;
+            /*checks if the queue is empty/ */
+            if (FRONT == -1)
+            {
+                Console.WriteLine("Queue is empty\n");
+                return;
+            }
+            Console.WriteLine("nElements in the queue are...............\n");
+            if (FRONT_position <= REAR_position) { }
+        }
     }
 }
