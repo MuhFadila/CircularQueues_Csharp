ca52375de5cc8d3ae93bd6dd45c793a136793a07
Muhammad Fadila <muh.fadila18@gmail.com>
2022-12-16 17:11:31 +0700

    create fungtion IF

--- Program.cs
+++ Program.cs
@@ -8,13 +8,26 @@ namespace CircularQueues_Csharp
         int[] queue_array = new int[5];
         public Queues()
         {
-            /* Initializing the values of the variebles REAR and FRONT to -1 to indicate that queue is initialy empty. */
+            /* Initializing the values of the variebles REAR and FRONT to -1 
+             * to indicate that queue is initialy empty. */
             FRONT = -1;
             REAR = -1;
         }
         public void insert(int element)
         {
-
+            /* This statement checks for the overflow condition. */
+            if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+            {
+                Console.WriteLine("\nQueue overflow\n");
+            }
+            /* The following statement checks whether the queue is empty. 
+             * if the queue is empty then value of REAR and FRONT variables 
+             * is set to 0. */
+            if (FRONT == -1)
+            {
+                FRONT = 0;
+                REAR = 0;
+            }
         }
     }
 }
