c570038ed5f4d4ac63865273094c4000740ca0e0
Muhammad Fadila <muh.fadila18@gmail.com>
2022-12-16 17:01:48 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CircularQueues_Csharp
+{
+    class Queues
+    {
+        int FRONT, REAR, max = 5;
+        int[] queue_array = new int[5];
+        public Queues() { }
+    }
+}
